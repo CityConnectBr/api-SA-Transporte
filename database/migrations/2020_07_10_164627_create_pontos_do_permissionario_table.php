590f4f2b087b1640ea0d5933bd3bd357b7f888b4
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2021-08-14 10:31:24 -0300

    Criação da migration ponto do permissionario

---
+++ database/migrations/2020_07_10_164627_create_pontos_do_permissionario_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePontosDoPermissionarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pontos_do_permissionario', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('permissionario_id')->unsigned()->nullable();
+            $table->integer('ponto_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->foreign('permissionario_id')->references('id')->on('permissionarios')->onDelete('cascade');
+            $table->foreign('ponto_id')->references('id')->on('pontos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pontos_do_permissionario');
+    }
+}
