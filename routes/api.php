3974475c3e8e079b434ea4210c2ea1e8f03d8672
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2021-09-04 20:59:40 -0300

    Correcao de nomeclatura

--- routes/api.php
+++ routes/api.php
@@ -45,7 +45,7 @@ Route::group([
                 Route::resource('/perfis', 'Admin\PerfilController');
                 Route::resource('/pontos', 'Admin\PontoController');
                 Route::resource('/modalidades', 'Admin\ModalidadeController');
-                Route::resource('/pontosdopermissionario', 'Admin\PontoDoUsuarioController');
+                Route::resource('/pontosdopermissionario', 'Admin\PontoDoPermissionarioController');
                 Route::resource('/usuarios', 'Admin\UsuarioController');
                 Route::get('/municipios/uf', 'Admin\MunicipioController@indexByUf');
                 Route::resource('/municipios', 'Admin\MunicipioController');
