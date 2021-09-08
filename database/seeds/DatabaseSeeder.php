258d0d9489552fe449414e691edbc2ba35e3d018
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2021-09-08 20:59:13 -0300

    Criação de alguns itens de seed para demonstração

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,13 +28,6 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('123456'),
         ]);*/
 
-        DB::table('permissionarios')->insert([
-            'nome_razao_social' => 'teste',
-            'tipo' => '1',
-            'cpf_cnpj' => '67958010800',
-            'cnh' => '123',
-        ]);
-
         for($i = 0;$i < 100;$i++){
             DB::table('municipios')->insert([
                 'nome' => "Municipio RJ $i",
@@ -56,6 +49,79 @@ class DatabaseSeeder extends Seeder
                 'capacidade_legal' => 'teste'
             ]);
         }
+
+        // DEMONSTRACAO
+        //permissionario
+        DB::table('enderecos')->insert([
+            'cep' => '27777-000',
+            'endereco' => 'Endereco Teste',
+            'numero' => '123',
+            'bairro' => 'Bairro',
+            'uf' => 'RJ',
+            'municipio_id' => 1,
+        ]);
+        DB::table('enderecos')->insert([
+            'cep' => '27777-000',
+            'endereco' => 'Endereco Teste2',
+            'numero' => '123',
+            'bairro' => 'Bairro',
+            'uf' => 'RJ',
+            'municipio_id' => 1,
+        ]);
+
+        DB::table('cores_veiculos')->insert([
+            'descricao' => 'Verde',
+            'id_integracao' => '321'
+        ]);
+
+        DB::table('marcas_modelos_veiculos')->insert([
+            'descricao' => 'City',
+            'id_integracao' => '321'
+        ]);
+
+        DB::table('permissionarios')->insert([
+            'nome_razao_social' => 'teste',
+            'tipo' => '1',
+            'cpf_cnpj' => '67958010800',
+            'cnh' => '123',
+        ]);
+
+        DB::table('usuarios')->insert([
+            'nome' => 'Admin',
+            'email' => 'teste2@teste.com',
+            'permissionario_id' => 1,
+            'tipo_id' => 1,
+            'password' => bcrypt('123456'),
+        ]);
+
+        DB::table('veiculos')->insert([
+            'situacao' => 'A',
+            'placa' => 'AAA2211',
+            'cod_renavam' => '321',
+            'ano_modelo' => '2010',
+            'permissionario_id' => 1,
+            'categoria_id' => 1,
+            'marca_modelo_veiculo_id' => 1,
+            'cor_id' => 1,
+            'versao' => 0,
+        ]);
+
+        DB::table('condutores')->insert([
+            'situacao' => 'A',
+            'nome' => 'Fulano',
+            'permissionario_id' => 1,
+            'endereco_id' => 1,
+            'versao' => 0,
+        ]);
+
+        DB::table('monitores')->insert([
+            'situacao' => 'A',
+            'nome' => 'Fulano Monitor',
+            'permissionario_id' => 1,
+            'endereco_id' => 2,
+            'versao' => 0,
+        ]);
+
 
     }
 }
