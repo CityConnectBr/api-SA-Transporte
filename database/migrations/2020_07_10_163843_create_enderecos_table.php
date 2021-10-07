b5279981f8ec44ea2c2e84052e46091ee84e1b4b
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2021-10-07 20:15:16 -0300

    Ajuste no tamanho de campos do endereco

--- database/migrations/2020_07_10_163843_create_enderecos_table.php
+++ database/migrations/2020_07_10_163843_create_enderecos_table.php
@@ -17,8 +17,8 @@ class CreateEnderecosTable extends Migration
             $table->increments('id');
             $table->string('cep', 9)->nullable();
             $table->string('endereco', 40)->nullable();
-            $table->string('numero', 5)->nullable();
-            $table->string('complemento', 15)->nullable();
+            $table->string('numero', 20)->nullable();
+            $table->string('complemento', 40)->nullable();
             $table->string('bairro', 100)->nullable();
             $table->string('uf', 2)->nullable();
             $table->integer('municipio_id')->unsigned()->nullable();
