df30673b95fbdb81f65904aad584ef9555ed5d71
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2021-12-26 13:46:08 -0300

    Adição de municipio

--- app/Http/Controllers/Integracao/PontoController.php
+++ app/Http/Controllers/Integracao/PontoController.php
@@ -3,6 +3,7 @@ namespace app\Http\Controllers\Integracao;
 
 use App\Http\Controllers\Integracao\IntegracaoController;
 use App\Models\Endereco;
+use App\Models\Municipio;
 use App\Models\Ponto;
 use Illuminate\Http\Request;
 use App\Utils\Util;
@@ -54,6 +55,10 @@ class PontoController extends IntegracaoController
 
         $endereco = new Endereco();
         $endereco->fill($request->all());
+        $municipios = Municipio::searchByUf($endereco->uf, $request['municipio']);
+        if(sizeof($municipios)>0){
+            $endereco->municipio_id = $municipios[0]->id;
+        }
         $endereco->save();
 
         $obj = new Ponto();
