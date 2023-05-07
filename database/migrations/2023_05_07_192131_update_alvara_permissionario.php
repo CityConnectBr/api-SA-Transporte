3cc1121f255d656aad795eb515343a30319ebd14
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2023-05-07 17:31:44 -0300

    Adição de migration UpdateAlvaraPermissionario e ajuste em UpdateInfracoes

--- database/migrations/2023_05_07_192131_update_alvara_permissionario.php
+++ database/migrations/2023_05_07_192131_update_alvara_permissionario.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateInfracoes extends Migration
+class UpdateAlvaraPermissionario extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class UpdateInfracoes extends Migration
      */
     public function up()
     {        
-        Schema::table('infracoes', function (Blueprint $table) {
+        Schema::table('alvara_do_permissionario', function (Blueprint $table) {
             $table->string('tipo_pagamento', 20)->nullable();
             $table->string('chave_pix', 200)->nullable();
             $table->string('codigo_pix', 200)->nullable();
@@ -42,7 +42,7 @@ class UpdateInfracoes extends Migration
      */
     public function down()
     {
-        Schema::table('infracoes', function (Blueprint $table) {
+        Schema::table('alvara_do_permissionario', function (Blueprint $table) {
             $table->dropColumn('tipo_pagamento');
             $table->dropColumn('chave_pix');
             $table->dropColumn('codigo_pix');
@@ -58,9 +58,9 @@ class UpdateInfracoes extends Migration
             $table->dropColumn('usuario_pagamento_id');
             $table->dropColumn('empresa_id');
 
-            $table->dropForeign('infrações_usuario_pagamento_id_foreign');
-            $table->dropForeign('infrações_empresa_id_foreign');
-            $table->dropForeign('infrações_fmp_id_foreign');
+            $table->dropForeign('alvara_do_permissionario_usuario_pagamento_id_foreign');
+            $table->dropForeign('alvara_do_permissionario_empresa_id_foreign');
+            $table->dropForeign('alvara_do_permissionario_fmp_id_foreign');
         });
     }
 }
