7e1378b4d7299a7a79177f1dde6a9d0cb780ddb5
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2024-01-20 15:42:51 -0300

    AJustes em migrations e parte de banco

--- database/migrations/2023_10_10_160500_update_curso_condutor2.php
+++ database/migrations/2023_10_10_160500_update_curso_condutor2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateCursoCondutor extends Migration
+class UpdateCursoCondutor2 extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2023_11_26_154717_alter_tipo_solicitacao_alteracao2.php
+++ database/migrations/2023_11_26_154717_alter_tipo_solicitacao_alteracao2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterTipoSolicitacaoAlteracao extends Migration
+class AlterTipoSolicitacaoAlteracao2 extends Migration
 {
     /**
      * Run the migrations.
